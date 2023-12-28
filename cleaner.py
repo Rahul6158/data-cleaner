aad1d38612d6ccf0cd4b1ad59673e8755c901b9b
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-12-29 00:54:43 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -165,7 +165,6 @@ multilingual, and vibrant app—your go-to for seamless translation and speech c
     '''
         
     st.markdown(project_description1, unsafe_allow_html=True)
-    ''' --- '''
     st.header("Info of the methods used in the code")
     ''' --- '''
     left_column, right_column = st.columns(2)
